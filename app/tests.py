af9286081a05c0dadf7b3d8b0fbd6172f33c9094
icancode23 <ernipunarora@gmail.com>
2017-07-22 22:04:24 +0530

    added tests

--- app/tests.py
+++ app/tests.py
@@ -9,9 +9,9 @@ class TestDimensionDetector(TestCase):
 ## testing the about endpoint for dimensiod detection	
 	def test_aboutEndpoint(self):
 		payload = {'url1':'https://scontent.fdel7-1.fna.fbcdn.net/v/t34.0-0/s261x260/20289803_1573727455990823_868522136_n.jpg?oh=b80a983392887776a1d3fa67979ca982&oe=5975BFFF' , 'url2':'https://scontent.fdel7-1.fna.fbcdn.net/v/t34.0-0/s261x260/20206272_1573727562657479_2084465416_n.jpg?oh=9a6ee62317cfec2f5fde95d7ae18dda8&oe=59759294'}
-		#payload = json.dumps(payload)
+		payload = json.dumps(payload)
 		print self.client.post.__doc__
-		response = self.client.post('/about', payload)
+		response = self.client.post('/about',data=payload,content_type='application/json')
 		print response.content
 		self.assertEqual(response.status_code,200)
 
