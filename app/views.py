6ea719c1dc290835a477932bbb0310b304333a67
sukhad-app <sukhad.anand@gmail.com>
2017-07-22 10:51:02 +0530

    Update views.py
    patch optimisation

--- app/views.py
+++ app/views.py
@@ -78,9 +78,18 @@ def fun(request):
 	print "The url is "+topimageurl
 	#load the image, convert it to grayscale, and blur it slightly
 	imagetop = url_to_image(topimageurl)
-	detect_coin = imagetop.copy()
-	gray = cv2.cvtColor(detect_coin, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (7, 7), 0)
+	src=imagetop.copy()
+	src =cv2.cvtColor(src,cv2.COLOR_BGR2HSV)
+	lower = np.array([40,100,100])
+	h2=120
+	s2=135
+	v2=135
+	upper = np.array([h2,s2,v2])
+	mask = cv2.inRange(imagetop, lower, upper)
+	output = cv2.bitwise_and(imagetop, imagetop, mask = mask)
+	#detect_coin = imagetop.copy()
+	#gray = cv2.cvtColor(output, cv2.COLOR_HSV2GRAY)
+	gray = cv2.GaussianBlur(output, (7, 7), 0)
 
 	# perform edge detection, then perform a dilation + erosion to
 	# close gaps in between object edges
@@ -89,174 +98,239 @@ def fun(request):
 	edged = cv2.erode(edged, None, iterations=1)
 
 	# find contours in the edge map
-	try:
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-		cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	# sort the contours from left-to-right and initialize the
+	# 'pixels per metric' calibration variable
+	(cnts, _) = contours.sort_contours(cnts)	
+	# loop over the contours individually
+	c1=cnts[0]
+	c1 = max(cnts, key = cv2.contourArea)
+	orig = imagetop.copy()
+	box = cv2.minAreaRect(c1)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	
+	# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
+
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
+
+	# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	#initialise pixel matrix
+	pixelsPerMetric = 2.3/dB
+	gray = cv2.cvtColor(imagetop, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+	# perform edge detection, then perform a dilation + erosion to
+	# close gaps in between object edges
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
+	
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	cnts = cnts[1]
+	c = max(cnts, key = cv2.contourArea)
+	orig = imagetop.copy()
+	box = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
 
-		# sort the contours from left-to-right and initialize the
-		# 'pixels per metric' calibration variable
-		(cnts, _) = contours.sort_contours(cnts)
-		print "Check point 1"
-		# loop over the contours individually
-		c1=cnts[0]
-		for c in cnts:
-			if(detect(c) == "circle"):
-				c1=c
-				break
-		#if no coin found
-		if(detect(c1)!="circle"):
-			#returning basic shape
-			print "NO coin Found "
-			box = {'boxName' : "Box 2"}
-			responseobj = json.dumps(box,indent=4)
-			return HttpResponse(responseobj,content_type="application/json")
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-		orig = imagetop.copy()
-		box = cv2.minAreaRect(c1)
-		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-		box = np.array(box, dtype="int")
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	
+	# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
 
-		# order the points in the contour such that they appear
-		# in top-left, top-right, bottom-right, and bottom-left
-		# order, then draw the outline of the rotated bounding
-		# box
-		box = perspective.order_points(box)
-		cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
 
-		# loop over the original points and draw them
-		for (x, y) in box:
-			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	
+	# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	# compute the size of the object
+	width = dA * pixelsPerMetric
+	length = dB * pixelsPerMetric
+	if(width>length):
+		temp=width
+		width=length
+		length=temp
+	imagefront = url_to_image(frontimageurl)
+	src=imagefront.copy()
+	src =cv2.cvtColor(src,cv2.COLOR_BGR2HSV)
+	lower = np.array([40,100,100])
+	h2=120
+	s2=135
+	v2=135
+	upper = np.array([h2,s2,v2])
+	mask = cv2.inRange(imagefront, lower, upper)
+	output = cv2.bitwise_and(imagefront, imagefront, mask = mask)
+	#detect_coin = imagetop.copy()
+	#gray = cv2.cvtColor(output, cv2.COLOR_HSV2GRAY)
+	gray = cv2.GaussianBlur(output, (7, 7), 0)
 
-		print "Checkpoint 2"
-		# unpack the ordered bounding box, then compute the midpoint
-		# between the top-left and top-right coordinates, followed by
-		# the midpoint between bottom-left and bottom-right coordinates
-		(tl, tr, br, bl) = box
-		(tltrX, tltrY) = midpoint(tl, tr)
-		(blbrX, blbrY) = midpoint(bl, br)
+	# perform edge detection, then perform a dilation + erosion to
+	# close gaps in between object edges
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
 
-		# compute the midpoint between the top-left and top-right points,
-		# followed by the midpoint between the top-righ and bottom-right
-		(tlblX, tlblY) = midpoint(tl, bl)
-		(trbrX, trbrY) = midpoint(tr, br)
+	# find contours in the edge map
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-		# compute the Euclidean distance between the midpoints
-		dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-		dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-		#initialise pixel matrix
-		pixelsPerMetric = 2.3/dB
-		c = max(cnts, key = cv2.contourArea)
-		orig = imagetop.copy()
-		box = cv2.minAreaRect(c)
-		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-		box = np.array(box, dtype="int")
+	# sort the contours from left-to-right and initialize the
+	# 'pixels per metric' calibration variable
+	(cnts, _) = contours.sort_contours(cnts)	
+	# loop over the contours individually
+	c1=cnts[0]
+	c1 = max(cnts, key = cv2.contourArea)
+	orig = imagefront.copy()
+	box = cv2.minAreaRect(c1)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
 
-		# order the points in the contour such that they appear
-		# in top-left, top-right, bottom-right, and bottom-left
-		# order, then draw the outline of the rotated bounding
-		# box
-		box = perspective.order_points(box)
-		cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-		# loop over the original points and draw them
-		for (x, y) in box:
-			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	
+	# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
 
-		print "Check point 3"
-		# unpack the ordered bounding box, then compute the midpoint
-		# between the top-left and top-right coordinates, followed by
-		# the midpoint between bottom-left and bottom-right coordinates
-		(tl, tr, br, bl) = box
-		(tltrX, tltrY) = midpoint(tl, tr)
-		(blbrX, blbrY) = midpoint(bl, br)
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
 
-		# compute the midpoint between the top-left and top-right points,
-		# followed by the midpoint between the top-righ and bottom-right
-		(tlblX, tlblY) = midpoint(tl, bl)
-		(trbrX, trbrY) = midpoint(tr, br)
+	# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	#initialise pixel matrix
+	pixelsPerMetric = 2.3/dB
+	gray = cv2.cvtColor(imagefront, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
+	# perform edge detection, then perform a dilation + erosion to
+	# close gaps in between object edges
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
+	
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[1]
+	c = max(cnts, key = cv2.contourArea)
+	orig = imagefront.copy()
+	box = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
 
-		# compute the Euclidean distance between the midpoints
-		dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-		dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-		# compute the size of the object
-		width = dA * pixelsPerMetric
-		length = dB * pixelsPerMetric
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-		#compute height
-		imagefront = url_to_image(frontimageurl)
-		gray = cv2.cvtColor(imagefront, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+	
+	# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
 
-		# perform edge detection, then perform a dilation + erosion to
-		# close gaps in between object edges
-		edged = cv2.Canny(gray, 50, 100)
-		edged = cv2.dilate(edged, None, iterations=1)
-		edged = cv2.erode(edged, None, iterations=1)
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
 
-		# find contours in the edge map
-		cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[1]
-		c = max(cnts, key = cv2.contourArea)
-		orig = imagefront.copy()
-		box = cv2.minAreaRect(c)
-		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-		box = np.array(box, dtype="int")
+	
+	# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	# compute the size of the object
+	width1 = dA * pixelsPerMetric
+	length1 = dB * pixelsPerMetric
+	if(width1<length1):
+		height=width1
+	else:
+		height=length1
+	boxNAme = ''
+	if(length<23 and width<35 and height<2):
+		boxNAme = 'Envelope 1'
+	elif(length<34 and width<18 and height<10):
+		boxNAme = 'Box 2'
+	elif(length<34 and width<32 and height<10):
+		boxNAme = 'Box 3'
+	elif(length<34 and width<32 and height<18):
+		boxNAme = 'Box 4'
+	elif(length<34 and width<32 and height<34):
+		boxNAme = 'Box 5'
+	elif(length<42 and width<36 and height<37):
+		boxNAme = 'Box 6'
+	elif(length<48 and width<40 and height<39):
+		boxNAme = 'Box 7'
+	else:
+		boxNAme = 'Box 4'
 
-		# order the points in the contour such that they appear
-		# in top-left, top-right, bottom-right, and bottom-left
-		# order, then draw the outline of the rotated bounding
-		# box
-		box = perspective.order_points(box)
-		cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+	box = {'boxName' : boxNAme}
+	box = json.loads(box)
 
-		# loop over the original points and draw them
-		for (x, y) in box:
-			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-
-		print "Check point 4"
-		# unpack the ordered bounding box, then compute the midpoint
-		# between the top-left and top-right coordinates, followed by
-		# the midpoint between bottom-left and bottom-right coordinates
-		(tl, tr, br, bl) = box
-		(tltrX, tltrY) = midpoint(tl, tr)
-		(blbrX, blbrY) = midpoint(bl, br)
-
-		# compute the Euclidean distance between the midpoints
-		dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-		height = dA * pixelsPerMetric
-		# return  HttpResponse("Parameters are "+length+height+width)
-		height = dA * pixelsPerMetric
-		boxNAme = ''
-		if(length<23 and width<35 and height<2):
-			boxNAme = 'Envelope 1'
-		elif(length<34 and width<18 and height<10):
-			boxNAme = 'Box 2'
-		elif(length<34 and width<32 and height<10):
-			boxNAme = 'Box 3'
-		elif(length<34 and width<32 and height<18):
-			boxNAme = 'Box 4'
-		elif(length<34 and width<32 and height<34):
-			boxNAme = 'Box 5'
-		elif(length<42 and width<36 and height<37):
-			boxNAme = 'Box 6'
-		elif(length<48 and width<40 and height<39):
-			boxNAme = 'Box 7'
-		else:
-			boxNAme = 'Box 4'
-
-		box = {'boxName' : boxNAme}
-		box = json.loads(box)
-
-		responseobj = json.dumps(box)
-
-	except Exception as e:
-		print "The exception is "
-		print e
-
+	responseobj = json.dumps(box)
 	return HttpResponse(responseobj)
 
 	
