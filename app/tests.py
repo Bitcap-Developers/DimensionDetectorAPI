89880e94d8575b12e575de637c4ded0d533e14ee
icancode23 <ernipunarora@gmail.com>
2017-09-15 13:35:45 +0100

    check

--- app/tests.py
+++ app/tests.py
@@ -8,7 +8,7 @@ class TestDimensionDetector(TestCase):
 
 ## testing the about endpoint for dimensiod detection	
 	def test_aboutEndpoint(self):
-		payload = {'url1':'https://scontent.fdel7-1.fna.fbcdn.net/v/t34.0-0/s261x260/20289803_1573727455990823_868522136_n.jpg?oh=b80a983392887776a1d3fa67979ca982&oe=5975BFFF' , 'url2':'https://scontent.fdel7-1.fna.fbcdn.net/v/t34.0-0/s261x260/20206272_1573727562657479_2084465416_n.jpg?oh=9a6ee62317cfec2f5fde95d7ae18dda8&oe=59759294'}
+		payload = {'url1':'https://scontent-lhr3-1.xx.fbcdn.net/v/t35.0-12/21640562_10203865554036793_129594673_o.jpg?oh=8ed44762a98254d86c7f9fb06c64ca82&oe=59BE33B0' , 'url2':'https://scontent-lhr3-1.xx.fbcdn.net/v/t35.0-12/21640416_10203865554676809_840353446_o.jpg?oh=0d21d305a85bdba615c00763c5649389&oe=59BE3C88'}
 		payload = json.dumps(payload)
 		response = self.client.post('/about',data=payload,content_type='application/json')
 		print response.content
